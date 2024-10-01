2b4eb3145ba2feddd84e4fe7535ef5e875fb71d1
Jirka Manak <41122948+manakjiri@users.noreply.github.com>
2024-10-01 13:39:16 +0200

    remove duplicated code

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,30 @@
   // Theme switch
   const body = document.body;
   const lamp = document.getElementById("mode");
-  console.log('here');
+  const preference = window.matchMedia("(prefers-color-scheme:dark)").matches ? "dark" : "light";
+  const setting = localStorage.getItem("theme");
 
   const setTheme = (state) => {
     if (state === "light") {
-      localStorage.setItem("theme", "light");
       body.removeAttribute("data-theme");
     } else if (state === "dark") {
-      localStorage.setItem("theme", "dark");
       body.setAttribute("data-theme", "dark");
     }
   };
 
+  if (setting !== null) {
+    setTheme(setting);
+  } else {
+    setTheme(preference);
+  }
+
   lamp.addEventListener("click", () => {
-    setTheme(document.body.getAttribute("data-theme") ? 'light' : 'dark');
+    const theme = document.body.getAttribute("data-theme") ? 'light' : 'dark'
+    localStorage.setItem("theme", theme);
+    setTheme(theme);
   });
+
+  setTimeout(() => body.classList.remove("notransition"), 75);
 
   // Blur the content when the menu is open
   const cbox = document.getElementById("menu-trigger");
